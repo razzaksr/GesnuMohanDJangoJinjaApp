d933aeff17e124f1ffc6b8056e9063b0691c279f
razzaksr <razzaksr@gmail.com>
2022-09-28 10:49:50 +0530

    insert and list done

---
+++ mypro/ebuddy/documents.py
@@ -0,0 +1,20 @@
+from datetime import date, datetime
+from mongoengine import *
+
+class Event(Document):
+    eveId=SequenceField()
+    eveName=StringField()
+    eveDate=DateField()
+    eveDepartment=StringField()
+    eveParticipants=StringField(required=False)
+    eveWinner=StringField(required=False)
+    
+    def __str__(self):
+        return "Event Information "+str(self.eveId)+" "+self.eveName+" "+str(self.eveDate)+" "+self.eveDepartment+" "+self.eveParticipants+" "+self.eveWinner+"\n"
+    
+    def initiate(self,nm="",dt=datetime.utcnow(),dept="",part="",win=""):
+        self.eveName=nm
+        self.eveDate=dt
+        self.eveDepartment=dept
+        self.eveParticipants=part
+        self.eveWinner=win
--- mypro/ebuddy/views.py
+++ mypro/ebuddy/views.py
@@ -1,3 +1,23 @@
+from datetime import datetime
+from django.http import HttpResponse
 from django.shortcuts import render
+from mongoengine import *
+from certifi import *
+from . import documents
 
+myCert=where()
+client=connect(host="mongodb+srv://razak:mohamed@cluster0.ptmlylq.mongodb.net/?retryWrites=true&w=majority",
+               db="poc",username="razak",password="mohamed",tlsCAFile=myCert)
 # Create your views here.
+
+def makeCreate(req):
+    eve=documents.Event()
+    eve.initiate(input("Event name please "),datetime.fromisoformat(input("event date YYYY-MM-DD")),input("Event organising department "))
+    #eve.initiate("Picconet11",datetime.fromisoformat("2022-12-20"),"CSE")
+    eve.save()
+    return HttpResponse('Event has added')
+
+def makeList(req):
+    mine=documents.Event.objects.all()
+    for x in mine:print(x)
+    return HttpResponse("Event has listed")
